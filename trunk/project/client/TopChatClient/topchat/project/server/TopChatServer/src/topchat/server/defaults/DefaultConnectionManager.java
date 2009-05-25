5b701fc6ce7d2d1fbcee193c48748118a60c06f0
oana.iancu7 <oana.iancu7@2f049fb2-08ab-11de-b993-7fcebbf76996>
2009-05-25 20:29:54 +0000

    added true client

---
+++ trunk/project/client/TopChatClient/topchat/project/server/TopChatServer/src/topchat/server/defaults/DefaultConnectionManager.java
@@ -0,0 +1,140 @@
+/**
+    TopChatServer 
+    Copyright (C) 2009 Laura Dragoi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package topchat.server.defaults;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Manages a connection between the server and a client
+ *
+ */
+public class DefaultConnectionManager 
+{
+	/** The current context of the connection */
+	protected DefaultContext context;
+	
+	/** The selection key associated with the connection */
+	protected SelectionKey	key	= null;	
+
+	private static Logger logger = Logger.getLogger(DefaultConnectionManager.class);
+		
+	/**
+	 * Obtain buffer used for reading on this connection
+	 * from the context
+	 * @return read buffer
+	 */
+	public ByteBuffer getReadBuffer()
+	{
+		return context.getReadBuffer();
+	}
+
+	/**
+	 * Obtain buffer used for writing on this connection
+	 * from the context
+	 * @return write buffer
+	 */
+	public ByteBuffer getWriteBuffer()
+	{
+		return context.getWriteBuffer();
+	}
+		
+	/**
+	 * Method called after write operation has occured
+	 */
+	public void processWrite() 
+	{
+		logger.debug("Written.");
+		
+		// inform the context
+		context.processWrite();
+	}
+	
+	/**
+	 * Method called after read operation has occured
+	 * @param rd byte array containing read data
+	 */
+	public void processRead(byte[] rd) 
+	{
+		String s = new String(rd);
+		
+		logger.debug("Received: " + s);
+		
+		// inform the context
+		context.processRead(rd);
+	}	
+	
+	/**
+	 * Set the key associated with this connection
+	 * @param key the value to be set for the key
+	 */
+	public void setKey(SelectionKey key) {
+		this.key = key;
+	}	
+	
+	/**
+	 * Method called to announce that the write buffer from the context
+	 * contains data that needs to be written
+	 */
+	public void registerForWrite()
+	{
+		logger.debug("Registered key for write");
+		
+		key.interestOps( key.interestOps() | SelectionKey.OP_WRITE );
+		key.selector().wakeup();
+	}
+	
+	/**
+	 * Method called to announce that the context is ready to read data
+	 * @deprecated connection is always ready for reading data
+	 */
+	public void registerForRead()
+	{
+		logger.debug("Registered key for read");
+		
+		key.interestOps( key.interestOps() | SelectionKey.OP_READ );
+		key.selector().wakeup();
+	}	
+	
+	
+	/**
+	 * Method called to stop reading data
+	 * @deprecated network module handles unregistering
+	 */
+	public void unregisterRead()
+	{
+		logger.debug("Unregister read");
+		
+		key.interestOps( key.interestOps() & (~SelectionKey.OP_READ) );
+		key.selector().wakeup();
+	}
+	
+	/**
+	 * Method called to stop writing data
+	 * @deprecated network module handles unregistering
+	 */	
+	public void unregisterWrite()
+	{
+		logger.debug("Unregister write");
+		
+		key.interestOps( key.interestOps() & (~SelectionKey.OP_WRITE) );
+		key.selector().wakeup();
+	}	
+}
---
+++ trunk/project/client/TopChatClient/topchat/project/server/TopChatServer/src/topchat/server/gui/ServerGui.java
@@ -0,0 +1,151 @@
+/**
+    TopChatServer 
+    Copyright (C) 2009 Laura Dragoi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package topchat.server.gui;
+
+import java.awt.BorderLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+
+import org.apache.log4j.Logger;
+
+import topchat.server.interfaces.Gui;
+import topchat.server.interfaces.GuiMediator;
+
+/**
+ * Server GUI implementation
+ */
+@SuppressWarnings("serial")
+public class ServerGui extends JPanel implements Gui, GuiConstants {
+
+	/** Connection to mediator */
+	@SuppressWarnings("unused")
+	private GuiMediator med = null;
+
+	/** List showing connected users */
+	private ListPanel usersPanel;
+	/** List showing existing rooms */	
+	private ListPanel roomsPanel;
+
+	/** Label used for showing application status */
+	private JLabel statusLabel;
+
+	private static Logger logger = Logger.getLogger(ServerGui.class);
+
+	/**
+	 * Initiates the GUI and connects it to the mediator
+	 * 
+	 * @param med
+	 */
+	public ServerGui(GuiMediator med) 
+	{
+		init();
+
+		setMediator(med);
+		med.setGui(this);
+
+		logger.info("GUI initiated");
+	}
+
+	/**
+	 * Creates the GUI frame
+	 */
+	private void build() {
+		JFrame frame = new JFrame(APP_NAME);
+		frame.setContentPane(this);
+		frame.setSize(APP_WIDTH, APP_HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void show() {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				build();
+			}
+		});
+	}
+
+	@Override
+	public void setMediator(GuiMediator med) {
+		this.med = med;
+	}
+
+	/**
+	 * Initializes the GUI
+	 */
+	private void init() {
+		usersPanel = new ListPanel(ListSelectionModel.SINGLE_SELECTION, 
+									"users", 
+									USERS_LIST_MIN_W,
+									USERS_LIST_MIN_H);
+
+		
+		
+		roomsPanel = new ListPanel(ListSelectionModel.SINGLE_SELECTION, 
+									"rooms", 
+									ROOMS_LIST_MIN_W,
+									ROOMS_LIST_MIN_H);
+
+		JSplitPane horizSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+											roomsPanel, usersPanel);
+		horizSplitPane.setResizeWeight(1);
+
+		statusLabel = new JLabel(DEFAULT_STATUS);
+
+		this.setLayout(new BorderLayout());
+		this.add(horizSplitPane, BorderLayout.CENTER);
+		this.add(statusLabel, BorderLayout.SOUTH);
+	}
+
+	@Override
+	public void setStatus(String msg) {
+		statusLabel.setText(msg);
+	}
+
+	@Override
+	public void addUser(String user) {
+		SwingUtilities.invokeLater(new AddingRunnable(user, usersPanel.getListModel()));
+	}
+
+	@Override
+	public void addRoom(String room) {
+		SwingUtilities.invokeLater(new AddingRunnable(room, roomsPanel.getListModel()));
+	}
+
+	private class AddingRunnable implements Runnable {
+		String str;
+		DefaultListModel model = null;
+
+		public AddingRunnable(String str, DefaultListModel model) {
+			this.str = str;
+			this.model = model;
+		}
+
+		public void run() {			
+			model.addElement(str);
+		}
+	}
+
+}
